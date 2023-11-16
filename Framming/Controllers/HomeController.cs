cb8c8ae57877acaf3f16cd29b4859ffaced134c0
Massagli <adrielmassagli@gmail.com>
2023-11-16 09:06:13 -0300

    First Commit

---
+++ Framming/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Framming.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult cad_func()
+        {
+
+            return View();
+        }
+        public ActionResult alterar_cad()
+        {
+
+            return View();
+        }
+        public ActionResult administrador()
+        {
+          
+            return View();
+        }
+        public ActionResult verpostadm()
+        {
+
+            return View();
+        }
+    }
+}
