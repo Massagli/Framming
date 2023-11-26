9e0deeb338429ae91a5384674f28a112181997a4
Massagli <adrielmassagli@gmail.com>
2023-11-25 22:27:08 -0300

    Add Calendar In Adm Page

--- Framming/App_Start/RouteConfig.cs
+++ Framming/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Framming
             routes.MapRoute(
                 name: "Default",
                 url: "{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "administrador", id = UrlParameter.Optional }
             );
         }
     }
