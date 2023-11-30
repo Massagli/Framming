4de644b1206b2016548899720c56b31a973e949c
Massagli <adrielmassagli@gmail.com>
2023-11-30 20:49:36 -0300

    Add Movies in Adm Page

---
+++ Framming/Scripts/adm.js
@@ -0,0 +1,22 @@
+﻿function Elemento() {
+
+    var divRes = document.getElementById("favor")
+
+
+
+    fetch("https://framming-api.onrender.com/users").then(resposta => {
+        return resposta.json()
+    }).then(corpo => {
+
+        for (pos = 0; pos <= corpo.length; pos++) {
+            var teste = document.createElement("p")
+            divRes.appendChild(teste)
+            teste.innerHTML = corpo[pos].nickUsuario
+        }
+
+
+
+        console.log(corpo.length)
+        console.log(corpo[corpo.length - 1])
+    })
+}
